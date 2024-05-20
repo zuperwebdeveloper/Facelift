1fe7a1d29e3c047593dbb6cc4f1d594ab3ed1e3e
KavirajRamasamy <kaviraj.r@zuper.co>
2024-05-20 23:13:29 +0530

    js fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
     build: {
       rollupOptions: {
         output: {
-          entryFileNames: 'assets/js/',
+          entryFileNames: 'assets/js/main.js',
           chunkFileNames: 'chunks/chunk.[hash].js',
           assetFileNames: ({
             name
