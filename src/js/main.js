d8cc9777cdd0e96246a8d5f9b6ee39f3485d3375
KavirajRamasamy <kaviraj.r@zuper.co>
2024-05-24 12:23:36 +0530

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -137,20 +137,3 @@ $(document).ready(function () {
 });
 
 
-var swiper = new Swiper(".testimonials", {
-  spaceBetween: 200,
-  centeredSlides: true,
-  slidesPerView: 1,
-  loop: true,
-  effect: "fade",
-  crossFade: 'true',
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-});
