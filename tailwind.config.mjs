3367489065ad6972ccbe3ac696c199982e5dfa36
KavirajRamasamy <kaviraj.r@zuper.co>
2024-05-30 17:16:33 +0530

    solution page component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'Jakarta': ['Plus Jakarta Sans'],
+			'Inter': ['Inter']
+		},
 		extend: {},
 
 	},
